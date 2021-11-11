01becfca1f0cabf378e0ca496de96e3f87aae241
Rafael <58118643+RafaelNassybullin@users.noreply.github.com>
2021-11-12 00:55:10 +0300

    initiall D

---
+++ src/assets/globalStyles.js
@@ -0,0 +1,27 @@
+import {createGlobalStyle} from 'styled-components'
+
+
+
+export const GlobalStyle = createGlobalStyle`
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-size: 1.181vw;
+    overflow-x: hidden;
+    font-family: Roboto, sans-serif;
+
+  }
+  
+  .padding-9vw {
+    padding: 9.722vw;
+  }
+
+
+
+
+`
